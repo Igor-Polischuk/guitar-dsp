cd312d50e670607ce0ce2ccaef6628d55aa18d4e
IgorPolischuk <ihor.polishyk@gmail.com>
2026-06-08 23:26:06 +0200

    update ring buffer

--- crates/dsp-core/src/dsp/helpers/ring_buffer.rs
+++ crates/dsp-core/src/dsp/helpers/ring_buffer.rs
@@ -1,30 +1,43 @@
-pub struct RingBuffer<const N: usize> {
-    buffer: [f32; N],
+pub struct RingBuffer<const N: usize, D> {
+    buffer: [D; N],
     write_ptr: usize,
 }
 
-impl<const N: usize> RingBuffer<N> {
+impl<const N: usize, D> RingBuffer<N, D>
+where
+    D: Default + Copy,
+{
     pub fn new() -> Self {
         assert!(N > 0, "Buffer size must be greater than 0");
         Self {
-            buffer: [0.0; N],
+            buffer: std::array::from_fn(|_| D::default()),
             write_ptr: 0,
         }
     }
 
-    pub fn push(&mut self, sample: f32) {
+    pub fn push(&mut self, sample: D) {
         self.buffer[self.write_ptr] = sample;
 
         self.write_ptr = (self.write_ptr + 1) % N;
     }
 
-    pub fn get(&self, delay: usize) -> f32 {
+    pub fn get(&self, delay: usize) -> D {
         if delay >= N {
-            return 0.0;
+            return D::default();
         }
 
         let read_ptr = (self.write_ptr + N - 1 - delay) % N;
 
         self.buffer[read_ptr]
+    }
+
+    pub fn get_ref(&self, delay: usize) -> &D {
+        if delay >= N {
+            return &self.buffer[0];
+        }
+
+        let read_ptr = (self.write_ptr + N - 1 - delay) % N;
+
+        &self.buffer[read_ptr]
     }
 }
